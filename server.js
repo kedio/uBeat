ba79db0b6ead40944de725c3e5f3e5272f260809
Alex Gervais <alex.gervais@mac.com>
2014-10-06 09:38:55 -0400

    Fix the url problem with a new server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/partials', express.static(__dirname + '/partials'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.all('/*', function(req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+console.log('serving on port 3000...');
+app.listen(3000);
