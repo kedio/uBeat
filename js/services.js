8dfce1a0dcae130c398e25aaf018ea657c4e0cec
Alex Gervais <alex.gervais@mac.com>
2014-10-08 15:11:17 -0400

    - remove hardcoded token

--- js/services.js
+++ js/services.js
@@ -4,7 +4,6 @@ var api = {
 };
 
 services.factory('APIService', function($http) {
-    $http.defaults.headers.common.Authorization = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NDMyZjk0YmQ5YzNlYzk1M2Q0Mzc2OTYiLCJleHAiOjE0MTI3MzE0Mjg1MDJ9.ZsMgiqg53HUkzwidJZ8dgeNjLjhtsXA21DfwsF8Ia_4';
     return {
         getArtist: function(id) {
             return $http.get(api.base + '/artists/' + id);
