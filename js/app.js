cb8b3fd40eb66092ff4ebdc4db2126e04d464207
Alex Gervais <alex.gervais@mac.com>
2014-10-08 15:09:56 -0400

    + Added basic Authentification interceptor and related routes

--- js/app.js
+++ js/app.js
@@ -5,15 +5,47 @@ ubeatcore.controller('mainController', function($scope, $state) {
 });
 
 
-ubeatcore.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+ubeatcore.config(function ($locationProvider, $httpProvider, $stateProvider) {
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
 
-    /*$stateProvider.state('album', {
-        url: "/album",
-        templateUrl: "/partials/album.html"
-    });*/
+    $httpProvider.interceptors.push('TokenInterceptor');
+
+    $stateProvider.state('public', {
+        abstract: true,
+        templateUrl: '<ui-view></ui-view>',
+        data: {
+            authentificationRequired: false
+        }
+    });
+    $stateProvider.state('user', {
+        abstract: true,
+        templateUrl: '<ui-view></ui-view>',
+        data: {
+            authentificationRequired: true
+        }
+    });
 });
+
+ubeatcore.run(function($rootScope, $state, AuthenticationService) {
+    $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
+        if (toState.data.accessLimited) {
+            console.log('access denied');
+            $rootScope.error = "Access denied";
+            event.preventDefault();
+
+            if(fromState.url === '^') {
+                if(AuthenticationService.isAuthenticated())
+                    $state.go('user.home');
+                else {
+                    $rootScope.error = null;
+                    $state.go('public.login');
+                }
+            }
+        }
+    });
+});
+
 
