4ac939190e445713d34dc3a76f7d774129bd9aa9
Alex Gervais <alex.gervais@mac.com>
2014-10-08 14:34:59 -0400

    Refactoring services, providing a base api url

--- js/services.js
+++ js/services.js
@@ -1,22 +1,29 @@
 var services = angular.module('services', []);
+var api = {
+    base: "http://localhost:3000"
+};
 
 services.factory('APIService', function($http) {
     $http.defaults.headers.common.Authorization = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NDMyZjk0YmQ5YzNlYzk1M2Q0Mzc2OTYiLCJleHAiOjE0MTI3MzE0Mjg1MDJ9.ZsMgiqg53HUkzwidJZ8dgeNjLjhtsXA21DfwsF8Ia_4';
     return {
         getArtist: function(id) {
-            return $http.get('http://localhost:3000/artists/' + id);
+            return $http.get(api.base + '/artists/' + id);
         },
 
         getAlbum:function(id){
-            return $http.get('http://localhost:3000/albums/'+id);
+            return $http.get(api.base + '/albums/'+id);
         },
 
         getAlbumsForArtist: function(artistName){
-            return $http.get('http://localhost:3000/search/albums?q='+ artistName);
+            return $http.get(api.base + '/search/albums/', {
+                params: {
+                    q: artistName
+                }
+            });
         },
 
         getAlbumsTracks: function(albumId){
-            return $http.get('http://localhost:3000/albums/'+albumId+'/tracks');
+            return $http.get(api.base + '/albums/'+albumId+'/tracks');
         }
     }
 });
