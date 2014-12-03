759b45b1588dab0a1530ea7812bea06048800896
KevinDion <dion.kevin.1@gmail.com>
2014-12-03 00:03:16 -0500

    Finished user with follow/unfollow functionality with css style

--- js/modules/userInfo.js
+++ js/modules/userInfo.js
@@ -12,6 +12,9 @@ userInfoModule.controller('userInfoController', function($scope, $stateParams, A
     APIService.getUserInfo($stateParams.userId).success(function (data, status, headers, config) {
         $scope.userInfo = data;
     });
+    APIService.getUserInfo($scope.user.id).success(function (data, status, headers, config) {
+        $scope.currentUser = data;
+    });
 
     APIService.getPlaylists().success(function(data, status, headers, config){
         $scope.userPlaylists = $.map($(data), function(playlist) {
@@ -22,12 +25,38 @@ userInfoModule.controller('userInfoController', function($scope, $stateParams, A
         });
     });
 
+    $scope.canFollow = function(){
+        return !$scope.isCurrentUser() && !inCurrentUserFollowList();
+    }
+
+    $scope.isCurrentUser = function(){
+        if($scope.userInfo && $scope.currentUser)
+            return $scope.userInfo.id == $scope.currentUser.id;
+        else
+            return false;
+    }
+
+    var inCurrentUserFollowList = function() {
+        var inFollowList = false;
+        if($scope.userInfo && $scope.currentUser){
+            $scope.currentUser.following.forEach(function(followingUser){
+                if(followingUser.id == $scope.userInfo.id)
+                    inFollowList = true;
+            });
+        }
+        return inFollowList;
+    }
+
     $scope.follow = function(userId){
-        APIService.followUser(userId);
+        APIService.followUser(userId).success(function (data, status, headers, config){
+            $scope.currentUser.following = data.following
+        });
     }
 
     $scope.unfollow = function(userId){
-        APIService.unfollowUser(userId);
+        APIService.unfollowUser(userId).success(function (data, status, headers, config){
+            $scope.currentUser.following = data.following
+        });
     }
 
 });
